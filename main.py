6f8342448405214a596b616e21c4f7743c5804e2
JR-espacial <jorge_alan_2001@hotmail.com>
2024-04-15 16:46:31 -0600

    add: file structure

---
+++ main.py
@@ -0,0 +1,16 @@
+import loadData
+import Model
+import train
+import test
+
+
+
+
+
+
+def main():
+    data = loadData.load_data()
+    model = Model.Bert(data)
+    train.trainModel(model, data)
+    test.testModel(model, data)
+
