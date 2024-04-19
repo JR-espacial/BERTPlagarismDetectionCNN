452a896f5eac47e8ec7f6fe901c39cea4ccdbcc9
DanielCruzAr <daniel_cruzar@outlook.com>
2024-04-19 17:23:33 -0600

    fixed matrix

--- relationsMatrix.py
+++ relationsMatrix.py
@@ -1,30 +1,20 @@
 import os
+filename = "fire14-source-code-training-dataset/SOCO14-java.qrel"
+file_directory = "fire14-source-code-training-dataset/java"
 
 def get_relations_matrix(filename, file_directory):
-    # Define the file names and their relations
+    # Get the list of files in the directory
+    file_list = os.listdir(file_directory)
+    matrix_size = len(file_list)
+    matrix = [[0] * matrix_size for _ in range(matrix_size)]
+
+        # Define the file names and their relations
     with open(filename, "r") as file:
-        file_relations = [line.strip().split() for line in file]
+        for line in file:
+            file_names = line.split(' ')
+            i = int(file_names[0].split('.')[0])
+            j = int(file_names[1].split('.')[0])
+            matrix[i][j] = 1
+            matrix[j][i] = 1
 
-        # Get the list of files in the directory
-        file_list = os.listdir(file_directory)
-
-        # Sort the file list
-        file_list.sort()
-
-        # Create an empty matrix
-        matrix_size = len(file_list)
-        matrix = [[0] * matrix_size for _ in range(matrix_size)]
-
-        # Populate the matrix
-        for i in range(matrix_size):
-            for j in range(matrix_size):
-                # Check if the files are related
-                if any(file_list[i] in relation for relation in file_relations):
-                    matrix[i][j] = 1
-                else:
-                    matrix[i][j] = 0
-        
-        return matrix
-    
-# "fire14-source-code-training-dataset/java"
-# fire14-source-code-training-dataset/SOCO14-java.qrel
+    return matrix
