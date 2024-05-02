e2b7199f6d57df91d22ca8971dad3b271923b57a
pau-cardoso <paulinacardoso7@gmail.com>
2024-05-02 17:46:39 -0600

    add: metrics and training confusion matrix

--- Model.py
+++ Model.py
@@ -14,8 +14,7 @@ def createModel(VOCAB_SIZE, EMBEDDING_DIM=70, MAX_SEQUENCE_LENGTH=27):
     ])
     return model
 
-def trainModel(data,labels,val_data, val_labels):
-
+def trainModel(data, labels, val_data, val_labels):
   vocab_size = getNumTokens()
 
   # Reshape the data remove the second dimension
@@ -26,22 +25,29 @@ def trainModel(data,labels,val_data, val_labels):
   model = createModel( VOCAB_SIZE = vocab_size)
   # Compile and train the model
   model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-  model.fit(data_reshaped, labels, epochs=30, batch_size=10, validation_data=(val_reshaped, val_labels))
+  history = model.fit(
+      data_reshaped,
+      labels,
+      epochs=30,
+      batch_size=10,
+      validation_data=(val_reshaped, val_labels)
+  )
 
   #save model
   model.save('RNN.keras')
 
-  plotModel(model)
-  # plot_confusion_matrix(val_labels, model.predict(val_reshaped))
+  plotModel(history)
+
+  train_predictions = (model.predict(data_reshaped) > 0.5).astype("int32")
+  plot_confusion_matrix(labels, train_predictions, title='Training Confusion matrix')
 
 
 
 # Plot model accuracy , loss
-def plotModel(model):
-    import matplotlib.pyplot as plt
-
+def plotModel(history):
     # Plot model accuracy
-    plt.plot(model.history.history['accuracy'])
+    plt.plot(history.history['accuracy'], label='train_accuracy')
+    plt.plot(history.history['val_accuracy'], label='val_accuracy')
     plt.title('Model accuracy')
     plt.ylabel('Accuracy')
     plt.xlabel('Epoch')
@@ -49,7 +55,8 @@ def plotModel(model):
     plt.show()
 
     # Plot model loss
-    plt.plot(model.history.history['loss'])
+    plt.plot(history.history['loss'], label='train_loss')
+    plt.plot(history.history['val_loss'], label='val_loss')
     plt.title('Model loss')
     plt.ylabel('Loss')
     plt.xlabel('Epoch')
@@ -57,13 +64,32 @@ def plotModel(model):
     plt.show()
 
 
-def plot_confusion_matrix(y_true, y_pred):
-    cm = confusion_matrix(y_true, y_pred)
+def plot_confusion_matrix(labels, predictions, title='Confusion matrix'):
+    cm = confusion_matrix(labels, predictions)
+    TN, FP, FN, TP = cm.ravel()
+
+    # Calculo de metricas
+    true_positive_rate = TP / (TP + FN)
+    false_positive_rate = FP / (FP + TN)
+    accuracy = TP+TN / (TP + TN + FP + FN)
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    f1_score = 2 * ((precision * recall) / (precision + recall))
+
+    print("\n--------- Métricas ---------")
+    print("Accuracy:", accuracy)
+    print('Precision:', precision)
+    print('Recall:', recall)
+    print('F1 Score:', f1_score)
+    print('True positive rate:', true_positive_rate)
+    print('False positive rate:', false_positive_rate)
+
+    # Graficar matriz de confusión
     plt.figure(figsize=(8, 6))
     sns.heatmap(cm, annot=True, fmt='d', cmap='Blues',
                 xticklabels=['Negative', 'Positive'],
                 yticklabels=['Negative', 'Positive'])
     plt.xlabel('Predicted labels')
     plt.ylabel('True labels')
-    plt.title('Confusion Matrix')
+    plt.title(title)
     plt.show()
--- test.py
+++ test.py
@@ -1,15 +1,17 @@
 from Model import plot_confusion_matrix
-from tensorflow.keras import models
 import tensorflow as tf
 import numpy as np
 
+
 def test_model(model, x_test, y_test):
   x_test = np.squeeze(x_test, axis=1)
+
   # Evaluar el modelo en el conjunto de prueba
   test_loss, test_acc = model.evaluate(x_test, y_test)
+
   # Print the test loss and accuracy
   print("Test Accuracy:", test_acc)
   print("Test Loss:", test_loss)
 
   test_predictions = (model.predict(x_test) > 0.5).astype("int32")
-  plot_confusion_matrix(y_test, test_predictions)
+  plot_confusion_matrix(y_test, test_predictions, title='Test Confusion Matrix')
